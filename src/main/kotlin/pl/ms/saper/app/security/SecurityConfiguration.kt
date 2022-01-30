d363411fe438162de2a1631544afe4ea6624751a
maropolo139 <marekseget@onet.pl>
2022-01-30 21:39:40 +0100

    feat(security): add anonymous user access

--- src/main/kotlin/pl/ms/saper/app/security/SecurityConfiguration.kt
+++ src/main/kotlin/pl/ms/saper/app/security/SecurityConfiguration.kt
@@ -3,12 +3,17 @@ package pl.ms.saper.app.security
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Scope
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl
+import org.springframework.security.authentication.AnonymousAuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
 import pl.ms.saper.app.security.MyUserDetails
 
 @EnableWebSecurity
@@ -28,11 +33,15 @@ class SecurityConfiguration: WebSecurityConfigurerAdapter() {
             .disable()
             .cors()
             .and()
+            .anonymous().principal("anonymous").authorities("ROLE_ANONYMOUS")
+            .and()
             .authorizeRequests()
             .antMatchers("/test")
             .permitAll()
-            .anyRequest()
+            .antMatchers("/AuthenticationTest")
             .authenticated()
+            .anyRequest()
+            .hasAnyAuthority("ANONYMOUS", "USER")
             .and()
             .httpBasic()
             .and()
@@ -55,4 +64,28 @@ class SecurityConfiguration: WebSecurityConfigurerAdapter() {
     @Bean
     @Scope("singleton")
     fun getPasswordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    @Scope("singleton")
+    fun getRolesHierarchy(): RoleHierarchy {
+        val rolesHierarchy: RoleHierarchyImpl = RoleHierarchyImpl()
+        rolesHierarchy.setHierarchy("""
+            ROLE_ADMIN > ROLE_USER
+            ROLE_USER > ROLE_ANONYMOUS
+        """.trimIndent())
+
+        return rolesHierarchy
+    }
+
+    @Bean
+    @Scope("singleton")
+    fun getAnonymousProvider() = AnonymousAuthenticationProvider("anonymous_access")
+
+
+    @Bean
+    @Scope("singleton")
+    fun getAnonymousFilter() = AnonymousAuthenticationFilter(
+        "anonymous_access", "anonymous", listOf(SimpleGrantedAuthority("ROLE_ANONYMOUS"))
+    )
+
 }
