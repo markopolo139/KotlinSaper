2cdcaf26ed50b03dacaad734a86d5115265ba38a
maropolo139 <marekseget@onet.pl>
2022-02-01 22:01:00 +0100

    feat(ExceptionHandler): create handler for new exceptions

--- src/main/kotlin/pl/ms/saper/web/exception/ExceptionHandler.kt
+++ src/main/kotlin/pl/ms/saper/web/exception/ExceptionHandler.kt
@@ -1,17 +1,22 @@
 package pl.ms.saper.web.exception
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.InternalAuthenticationServiceException
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import pl.ms.saper.app.exceptions.InvalidPasswordException
 import pl.ms.saper.app.exceptions.InvalidUserException
 import java.lang.Exception
 import javax.servlet.http.HttpServletRequest
 
+@Suppress("UNCHECKED_CAST")
 @ControllerAdvice
 class ExceptionHandler: ResponseEntityExceptionHandler() {
 
@@ -30,9 +35,38 @@ class ExceptionHandler: ResponseEntityExceptionHandler() {
         apiSubErrors: List<ApiSubError> = emptyList()
     ): ResponseEntity<ApiError> = ApiError(suggestedAction, errorMessage, httpStatus, apiSubErrors).toResponseEntity()
 
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+
+        val subApiErrors: List<ApiSubError> = ex.bindingResult.fieldErrors.asSequence().map {
+            ApiSubError(
+                error = "validation failed for ${it.rejectedValue} - ${it.defaultMessage}",
+                suggestedAction = "Check rejected value"
+            )
+        }.toList()
+
+        return error(
+            suggestedAction = "Check error sublist for more information",
+            errorMessage = "Error occurred during validation",
+            apiSubErrors = subApiErrors,
+            httpStatus = HttpStatus.BAD_REQUEST
+        ) as ResponseEntity<Any>
+    }
+
     @ExceptionHandler(InvalidUserException::class)
     fun invalidUserExceptionHandler(ex: InvalidUserException): ResponseEntity<ApiError> = error(
         suggestedAction = "Selected user does not exist, register before login",
+        errorMessage = ex.message ?: DEFAULT_ERROR_MESSAGE,
+        httpStatus = HttpStatus.BAD_REQUEST
+    )
+
+    @ExceptionHandler(InvalidPasswordException::class)
+    fun invalidPasswordExceptionHandler(ex: InvalidPasswordException): ResponseEntity<ApiError> = error(
+        suggestedAction = "Typed password is not correct, check error message",
         errorMessage = ex.message ?: DEFAULT_ERROR_MESSAGE,
         httpStatus = HttpStatus.BAD_REQUEST
     )
