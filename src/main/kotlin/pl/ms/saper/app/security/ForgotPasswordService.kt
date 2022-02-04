3271cc25c638de3b6052e11218d15dd7471dd533
maropolo139 <marekseget@onet.pl>
2022-02-04 11:17:51 +0100

    feat(ForgotPassword): create controller and service
    -- only sendMessage

---
+++ src/main/kotlin/pl/ms/saper/app/security/ForgotPasswordService.kt
@@ -0,0 +1,83 @@
+package pl.ms.saper.app.security
+
+import net.bytebuddy.utility.RandomString
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.MailException
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.thymeleaf.ITemplateEngine
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
+import org.thymeleaf.spring5.ISpringTemplateEngine
+import org.thymeleaf.util.StringUtils
+import pl.ms.saper.app.data.repositories.UserRepository
+import pl.ms.saper.app.exceptions.InvalidUserEmailException
+import pl.ms.saper.app.exceptions.SendingEmailException
+import java.io.File
+
+@Service
+class ForgotPasswordService {
+
+    companion object {
+        const val EMAIL_PATH = "/api/v1/reset/password?token="
+        const val EMAIL_FROM = "springtest1@onet.pl"
+        const val IMAGE_PATH = "../static/forgotPasswordImage.png"
+    }
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+
+    @Autowired
+    private lateinit var mailSender: JavaMailSender
+
+    @Autowired
+    private lateinit var templateEngine: TemplateEngine
+
+    //TODO: when learn jwt, token also generate via jwt
+    fun sendMessage(email: String, serverPath: String) {
+
+        val userEntity = userRepository.findByEmail(email).orElseThrow { throw InvalidUserEmailException(email) }
+        with(userEntity) {
+            passwordToken = passwordToken ?: RandomString.make(100)
+        }
+
+        sendMail(email, "$serverPath$EMAIL_PATH${userEntity.passwordToken}")
+
+        userRepository.save(userEntity)
+    }
+
+    fun sendMail(email: String, hostPath: String) {
+
+        val context = Context()
+        context.setVariable("sendPath", hostPath)
+
+        val mimeMessage = mailSender.createMimeMessage()
+
+        try {
+            val mimeMessageHelper = MimeMessageHelper(mimeMessage, true)
+
+            mimeMessageHelper.setFrom(EMAIL_FROM, "Spring assistant")
+            mimeMessageHelper.setTo(email)
+            mimeMessageHelper.setSubject("Password Recovery")
+
+            mimeMessageHelper.addInline("image", File("src/main/kotlin/resources/static/forgotPasswordImage.png"))
+            mimeMessageHelper.setText(
+                templateEngine.process("email", context), true
+            )
+
+            mailSender.send(mimeMessage)
+        }
+        catch (ex: MailException) { throw SendingEmailException() }
+
+    }
+
+    fun changePassword(token: String, newPassword: String) {
+
+    }
+
+}
--- src/main/kotlin/pl/ms/saper/web/security/ForgotPasswordController.kt
+++ src/main/kotlin/pl/ms/saper/web/security/ForgotPasswordController.kt
@@ -1,4 +1,36 @@
 package pl.ms.saper.web.security
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import pl.ms.saper.app.security.ForgotPasswordService
+import javax.servlet.http.HttpServletRequest
+import javax.validation.Valid
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+
+@CrossOrigin
+@RestController
+@Validated
 class ForgotPasswordController {
+
+    @Autowired
+    private lateinit var forgotPasswordService: ForgotPasswordService
+
+
+    @PostMapping("/api/v1/send/message")
+    fun sendMessage(
+        @RequestParam @Email email: String,
+        request: HttpServletRequest
+    ) = forgotPasswordService.sendMessage(email, request.requestURL.toString().replace(request.servletPath, ""))
+
+    @PostMapping("/api/v1/reset/password")
+    fun resetPassword(
+        @RequestParam @NotBlank token: String,
+        @RequestParam @NotBlank password: String
+    ) = forgotPasswordService.changePassword(token, password)
+
 }
