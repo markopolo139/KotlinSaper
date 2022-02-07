66c8faaad344761b5e2fafb62f9e8b25570d13fd
maropolo139 <marekseget@onet.pl>
2022-02-07 20:49:23 +0100

    feat(test): implement equals and hash code for spot mock

--- src/test/kotlin/pl/ms/saper/business/mock/SpotMock.kt
+++ src/test/kotlin/pl/ms/saper/business/mock/SpotMock.kt
@@ -9,4 +9,29 @@ class SpotMock(
     override var isMined: Boolean,
     override var isFlagged: Boolean,
     override var minesAround: Int
-) : Spot
+) : Spot {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SpotMock
+
+        if (position != other.position) return false
+        if (isChecked != other.isChecked) return false
+        if (isMined != other.isMined) return false
+        if (isFlagged != other.isFlagged) return false
+        if (minesAround != other.minesAround) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = position.hashCode()
+        result = 31 * result + isChecked.hashCode()
+        result = 31 * result + isMined.hashCode()
+        result = 31 * result + isFlagged.hashCode()
+        result = 31 * result + minesAround
+        return result
+    }
+}
