2386dcd7fe9d1a7fd3fbedb10afbf498d65f78b0
maropolo139 <marekseget@onet.pl>
2022-02-10 19:11:55 +0100

    feat(schema): change configuration schema in database

--- schema/database.sql
+++ schema/database.sql
@@ -27,19 +27,29 @@ CREATE TABLE spots (
     mines_around int not null
 );
 
-CREATE TABLE `configuration` (
-    configuration_id int not null primary key auto_increment,
-    height int not null,
-    width int not null,
-    mines int not null
-);
-
 CREATE TABLE boards (
     board_id int not null primary key auto_increment,
     user_id int not null,
+    constraint foreign key(user_id) references app_users(user_id)
+);
+
+CREATE TABLE `configuration` (
+    configuration_id int not null primary key auto_increment,
+    board_id int not null,
+    constraint foreign key(board_id) references boards(board_id)
+);
+
+CREATE TABLE configuration_entry (
+    configuration_entry_id int not null primary key auto_increment,
+    entry_name varchar(128) not null,
+    `value` varchar(128) not null
+);
+
+CREATE TABLE config_to_entry (
     configuration_id int not null,
-    constraint foreign key(user_id) references app_users(user_id),
-    constraint foreign key(configuration_id) references `configuration`(configuration_id)
+    configuration_entry_id int not null,
+    constraint foreign key(configuration_id) references `configuration`(configuration_id),
+    constraint foreign key(configuration_entry_id) references configuration_entry(configuration_entry_id)
 );
 
 INSERT INTO app_users VALUES(1, "guest", "$2a$12$0OTUMsGYKjMRWKYq6ecHpObjEj9/9utZm6kPRGzx1yasJpSo3PO3y", "email", null);
